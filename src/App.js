f7a8a12e7d71695b37056780d3618c1d2619aa56
chenuka <chenukadanith@gmai.com>
2024-02-16 11:28:24 +0530

    a12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+
+
+
 import './App.css';
+import Home from './Home';
+import Navbar from './component/common/NavBar';
+import StudentsView from './component/student/StudentsView';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import NavBar from './component/common/NavBar';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <NavBar/>
+      
+      <StudentsView/>
+      
     </div>
   );
 }
